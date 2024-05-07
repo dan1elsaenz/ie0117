d9667cf367d7ca42d763945846c8bf6742c91fb0
DanielSaenz11 <dsaenzobando@gmail.com>
2024-05-06 23:16:40 -0600

    Fixed header and commented

--- Laboratorio3/ej1.c
+++ Laboratorio3/ej1.c
@@ -1,3 +1,20 @@
+/*
+ * Laboratorio 3 IE-0117
+ * Ejercicio 1
+ * Daniel Saenz Obando - C37099
+ */
+
+// Header utilizado
+#include <stdio.h>
+
+/*
+ * main() calcula los primeros 100 numeros triangulares
+ * por medio de un for loop que añade el indice del bucle
+ * a una variable total definida inicialmente
+ * 
+ * @return 0 si el codigo se ejecuto correctamente
+ */
+
 int main() {
     int total = 0;
 
