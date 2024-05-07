5470148b26042266677843af4ac093564f7f9ecf
DanielSaenz11 <dsaenzobando@gmail.com>
2024-05-06 23:10:45 -0600

    Added code for ej1.c

---
+++ Laboratorio3/ej1.c
@@ -0,0 +1,11 @@
+int main() {
+    int total = 0;
+
+    for (int index = 0; index<100; index++) {
+        total += index;
+        printf("%d ", total);
+    }
+    printf("\n"); // Salto de linea al final
+
+    return 0;
+}
