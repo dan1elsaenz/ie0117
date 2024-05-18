a18bbe92df8d9223f966a925bc257d275a6b77d3
DanielSaenz11 <dsaenzobando@gmail.com>
2024-05-17 21:34:43 -0600

    Documented code and changed variables names

--- Laboratorio4/ej2.c
+++ Laboratorio4/ej2.c
@@ -1,4 +1,18 @@
+/*
+ * Laboratorio 4 - IE-0117
+ * Ejercicio 2
+ * Daniel Saenz Obando - C37099
+ */
+
+// Biblioteca
 #include <stdio.h>
+
+/*
+ * encontrarMinimo() busca el menor numero por medio de una busqueda lineal
+ * @param1 array[] Arreglo de numeros enteros
+ * @param2 longitud Largo del arreglo
+ * @return numeroMenor
+ */
 
 int encontrarMinimo(int array[], int longitud) {
     int numeroMenor = array[0];
@@ -12,6 +26,13 @@ int encontrarMinimo(int array[], int longitud) {
     return numeroMenor;
 }
 
+
+/*
+ * encontrarMaximo() busca el mayor numero por medio de una busqueda lineal
+ * @param1 array[] Arreglo de numeros enteros
+ * @param2 longitud Largo del arreglo
+ * @return numeroMayor
+ */
 int encontrarMaximo(int array[], int longitud) {
     int numeroMayor = array[0];
 
@@ -23,6 +44,16 @@ int encontrarMaximo(int array[], int longitud) {
 
     return numeroMayor;
 }
+
+/* 
+ * encontrarMinMax_ptr() encuentra el numero maximo y minimo por medio de una
+ * busqueda lineal utilizando punteros
+ * @param1 * arreglo Puntero al primer elemento del arreglo de numeros enteros
+ * @param2 * minimo Puntero a la variable minimo_ptr del main
+ * @param3 * maximo Puntero a la variable maximo_ptr del main
+ * @param4 longitud Entero que contiene el largo del arreglo
+ * @return No retorna ningun valor
+ */
 
 void encontrarMinMax_ptr(int * arreglo, int * minimo, int * maximo, int longitud) {
     *minimo = *arreglo;
@@ -42,11 +73,18 @@ void encontrarMinMax_ptr(int * arreglo, int * minimo, int * maximo, int longitud
 }
 
 
+/*
+ * main()
+ * Punto de entrada al programa donde se ejecuta la logica principal
+ * Se llaman las funciones anteriores
+ * @return 0 si no ocurrieron errores
+ */
 int main() {
     int array[] = {21, 24, 65, -3, 56, 73, 35, 15};
 
     int longitud = sizeof(array)/sizeof(array[0]);
 
+    // Definicion de las variables empleadas en el programa
     int minimo = 0;
     int maximo = 0;
     int minimo_ptr = 0;
