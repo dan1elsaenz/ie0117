df3e820cc2b607268ed31c5f4489054528fa2a86
DanielSaenz11 <dsaenzobando@gmail.com>
2024-06-03 22:46:59 -0600

    Added functions insertarIndex() and eliminarElemento()

--- Laboratorio5/double_list.c
+++ Laboratorio5/double_list.c
@@ -63,3 +63,100 @@ void insertarFinal(Node* head, int data) {
     actual->next = nodoFinal;
     nodoFinal->prev = actual;
 }
+
+void insertarIndex(Node** head, int data, int posicion) {
+    if (head == NULL) {
+        printf("Error: Puntero a nodo head invalido\n");
+        return;
+    }
+
+    else if (posicion < 0) {
+        printf("Error: %d no es una posicion valida entera positiva\n", posicion);
+        return;
+    }
+
+    else if (posicion == 0) {
+        insertarInicio(head, data);
+        return;
+    }
+
+    else if (*head == NULL) {
+        printf("Error: Nodo head no esta definido y no se puede agregar elemento en la posicion %d\n", posicion);
+        return;
+    }
+
+    int contador = 0;
+    Node* actual = *head;
+
+    while (contador != posicion && actual->next != NULL) {
+        actual = actual->next;
+
+        contador++;
+    }
+
+    if (contador+1 == posicion) {
+        insertarFinal(*head, data);
+        return;
+    }
+
+    else if (contador+1 < posicion) {
+        printf("Error: El indice del ultimo nodo es %d. La posicion %d ingresada esta fuera de los limites de la lista\n", contador, posicion);
+        return;
+    }
+
+    Node* nuevoNodo = crearNodo(data);
+
+    if (nuevoNodo == NULL) {
+        printf("Error: No se pudo reservar memoria para el nuevo nodo\n");
+        return;
+    }
+
+    nuevoNodo->prev = actual->prev;
+    actual->prev->next = nuevoNodo;
+    actual->prev = nuevoNodo;
+    nuevoNodo->next = actual;
+
+}
+
+void eliminarElemento(Node** head, int dataEliminar) {
+    
+    if (head == NULL) {
+        printf("Error: Puntero a nodo head invalido\n");
+        return;
+    }
+    
+    else if (*head == NULL) {
+        printf("Error: Nodo head no definido\n");
+        return;
+    }
+    
+    Node* actual = *head;
+    
+    while (actual != NULL) {
+        if (actual->data == dataEliminar) {
+            break;
+        }
+        
+        actual = actual->next;
+    }
+
+    if (actual == NULL) {
+        printf("No se encontro el nodo con %d en su data\n", dataEliminar);
+        return;
+    }
+
+    else if (actual->prev == NULL) {
+        *head = actual->next;
+    }
+
+    else if (actual->next == NULL) {
+        actual->prev->next = NULL;
+    }
+    else {
+        actual->next->prev = actual->prev;
+        actual->prev->next = actual->next;
+    }
+
+    free(actual);
+    actual = NULL;
+}
