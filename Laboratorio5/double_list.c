583ccaca54c5c278d89f97194743ba5066b2d77c
DanielSaenz11 <dsaenzobando@gmail.com>
2024-06-03 22:22:02 -0600

    Code for InsertarInicio() function

---
+++ Laboratorio5/double_list.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "double_list.h"
+
+// Evitar repeticion del codigo
+Node* crearNodo(int data) {
+    Node* nodoNuevo = (Node *)malloc(sizeof(Node));
+
+    if (nodoNuevo == NULL) {
+        printf("Error: No se pudo crear el nodo\n");
+        return NULL;
+    }
+
+    nodoNuevo->data = data;
+    nodoNuevo->prev = NULL;
+    nodoNuevo->next = NULL;
+ 
+    return nodoNuevo;
+}
+
+void insertarInicio(Node **head, int data) {
+    if (head == NULL) {
+        printf("Error: Puntero a nodo head invalido\n");
+        return;
+    }
+    
+    Node* nodoInicial = crearNodo(data);
+
+    if (nodoInicial == NULL) {
+        printf("Error: No se pudo reservar memoria para el nuevo nodo\n");
+        return;
+    }
+    
+    if (*head == NULL) {
+        *head = nodoInicial;
+        return;
+    }
+    
+    nodoInicial->next = *head;
+    (*head)->prev = nodoInicial;
+    *head = nodoInicial;
+}
