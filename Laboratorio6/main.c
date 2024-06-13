d4bf68b634be663ffc8e2c0c543cece4ffc265ed
DanielSaenz11 <dsaenzobando@gmail.com>
2024-06-12 20:35:49 -0600

    Fixed format

--- Laboratorio6/main.c
+++ Laboratorio6/main.c
@@ -7,13 +7,24 @@ int main() {
     printf("%d\n", isEmpty(stack));
 
     push(stack, 1);
+
+    printStack(stack);
+
     push(stack, 2);
+    
+    printStack(stack);
+
+
     push(stack, 3);
+    printStack(stack);
     push(stack, 4);
+    printStack(stack);
 
     int popped = pop(stack);
 
     printf("%d\n", popped);
+
+    printStack(stack);
 
     printf("%d\n", isEmpty(stack));
 
