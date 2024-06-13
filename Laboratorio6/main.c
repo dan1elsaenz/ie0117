b3e2fa56bb706092e19e0b4e8596aa9d3b31bedd
DanielSaenz11 <dsaenzobando@gmail.com>
2024-06-12 22:18:06 -0600

    Add print staments and include all funcs

--- Laboratorio6/main.c
+++ Laboratorio6/main.c
@@ -2,9 +2,18 @@
 #include <stdio.h>
 
 int main() {
+    int estaVacio = 0;
     Stack* stack = initStack();
 
-    printf("%d\n", isEmpty(stack));
+    estaVacio = isEmpty(stack);
+
+    if (!estaVacio) {
+        printf("Stack no vacio\n");
+    }
+    else if (estaVacio == 1) {
+        printf("Stack vacio\n");
+    }
+    
 
     push(stack, 1);
 
@@ -14,22 +23,30 @@ int main() {
     
     printStack(stack);
 
-
     push(stack, 3);
+
     printStack(stack);
+
     push(stack, 4);
-    printStack(stack);
-
-    int popped = pop(stack);
-
-    printf("%d\n", popped);
 
     printStack(stack);
 
-    printf("%d\n", isEmpty(stack));
+    int poppedData = pop(stack);
+
+    printf("Data extraido: %d\n", poppedData);
+
+    printStack(stack);
+
+    estaVacio = isEmpty(stack);
+
+    if (!estaVacio) {
+        printf("Stack no vacio\n");
+    }
+    else if (estaVacio == 1) {
+        printf("Stack vacio\n");
+    }
 
     freeStack(stack);
-
 
     return 0;
 }
